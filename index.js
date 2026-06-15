14a21d04ef03ddae5b12ca418e24e559ae2d1fd1
Jaaja Forty <jaajaforty@gmail.com>
2026-06-15 02:25:56 -0400

    last version

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ app.get("/logout",(req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running `);
 });
